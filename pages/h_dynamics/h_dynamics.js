e06fa33ee136972ba577ca1e3772a35ec525825b
matchstick9505 <1749330778@qq.com>
2019-12-13 09:30:53 +0800

    版本维护
    版本维护

---
+++ pages/h_dynamics/h_dynamics.js
@@ -0,0 +1,49 @@
+// pages/hdynamics/hdynamics.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.hdynamics();
+  },
+  hdynamics() {
+    wx.showToast({
+      title: '正在加载',
+      icon: 'loading',
+      duration: 10000
+    })
+    var that = this;
+
+    wx.request({
+      url: 'http://localhost:8081/SelectNew',
+      method: 'post',
+      header: {
+        'content-type': 'application/SelectNew' // 默认值
+      },
+      success: function (res) {
+        var list = res.data;
+        if (list == null) {
+          wx.showToast({
+            title: 'ErrorMessage',
+            icon: 'false',   //图标
+            duration: 1500  //提示的延迟的时间
+          })
+        } else {
+          that.setData({
+            list: list
+          })
+          console.log(res)
+          wx.hideToast()
+        }
+      }
+    })
+  }
+})
